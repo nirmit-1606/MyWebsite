2864fb4c02ab025aa89a102fa6c837dce9e8aa1f
Nirmit P <nirmitpatel16620@gmail.com>
2025-06-03 12:04:37 -0500

    update card details

--- restaurant-menu-app/src/components/RecipeCard.js
+++ restaurant-menu-app/src/components/RecipeCard.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './RecipeCard.css';
 
 const RecipeCard = ({ recipe }) => {
-    console.log(recipe);
+  const {
+    image,
+    name,
+    cuisine,
+    rating
+  } = recipe;
+
   return (
     <div className="recipe-card">
-      <img src={recipe.image} alt={recipe.name} />
-      <h3>{recipe.name}</h3>
-      <p><strong>Prep Time:</strong> {recipe.prepTimeMinutes} mins</p>
-      <p><strong>Servings:</strong> {recipe.servings}</p>
+      <img src={image} alt={name} className="recipe-image" />
+      <div className="recipe-details">
+        <h3>{name}</h3>
+        <p>{cuisine} | ⭐ {rating}</p>
+      </div>
     </div>
   );
 };
