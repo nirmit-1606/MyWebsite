0e5e7d505d96ec5d77317981ebe0b55bfb715b06
Nirmit P <nirmitpatel16620@gmail.com>
2025-06-03 13:55:07 -0500

    cart icon animation on changing cart items

--- restaurant-menu-app/src/components/CartIcon.js
+++ restaurant-menu-app/src/components/CartIcon.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ShoppingCart } from 'lucide-react';
 import './CartIcon.css';
 
@@ -8,8 +8,18 @@ const CartIcon = () => {
   const cart = useSelector((state) => state.cart);
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
+  const [animate, setAnimate] = useState(false);
+
+  useEffect(() => {
+    if (totalItems > 0) {
+      setAnimate(true);
+      const timer = setTimeout(() => setAnimate(false), 500);
+      return () => clearTimeout(timer);
+    }
+  }, [totalItems]);
+
   return (
-    <Link to="/cart" className="cart-icon" title="View Cart">
+    <Link to="/cart" className={`cart-icon ${animate ? 'bounce' : ''}`} title="View Cart">
       <ShoppingCart size={24} />
       {totalItems > 0 && <span className="item-count">{totalItems}</span>}
     </Link>
