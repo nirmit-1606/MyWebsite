6d24c910eb326eadae83f2a59407acb9238d36c3
Nirmit P <nirmitpatel16620@gmail.com>
2025-06-03 13:43:21 -0500

    use lucide icon for cart

--- restaurant-menu-app/src/components/CartIcon.js
+++ restaurant-menu-app/src/components/CartIcon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'lucide-react';
 import './CartIcon.css';
 
 const CartIcon = () => {
@@ -8,8 +9,8 @@ const CartIcon = () => {
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
-    <Link to="/cart" className="cart-icon">
-      🛒
+    <Link to="/cart" className="cart-icon" title="View Cart">
+      <ShoppingCart size={24} />
       {totalItems > 0 && <span className="item-count">{totalItems}</span>}
     </Link>
   );
